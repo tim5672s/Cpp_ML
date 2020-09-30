c60ce8d40e5cd92ec9ed172093213a30a1d92d74
Tim Krebs <timkrebs@MacBook-Pro.fritz.box>
2020-09-30 16:57:03 +0200

    added setter header

--- mnist_ml/include/knn.hpp
+++ mnist_ml/include/knn.hpp
@@ -1,15 +1,12 @@
 #ifndef __KNN_H
 #define __KNN_H
 
-#include <vector>
-#include "data.hpp"
+#include "setter.hpp"
 
-class knn{
+class knn : public setter_data{
     int k;
     std::vector<data *> *neighbors;
-    std::vector<data *> *training_data;
-    std::vector<data *> *test_data;
-    std::vector<data *> *validation_data;
+    
 
     public:
         knn(int);
@@ -17,11 +14,7 @@ class knn{
         ~knn();
 
         void find_knearest(data *query_point);
-        void set_training_data(std::vector<data *> *vect);
-        void set_test_data(std::vector<data *> *vect);
-        void set_validation_data(std::vector<data *> *vect);
         void set_k(int val);
-
         int predict();
         double calculate_distance(data *query_point, data *input);
         double validate_performance();
