88e7aabf738637f16209d8b53a15cb4ad8094735
Tim Krebs <timkrebs@MacBook-Pro.fritz.box>
2020-09-30 15:09:31 +0200

    fixed get_feature_vector

--- mnist_ml/src/data.cpp
+++ mnist_ml/src/data.cpp
@@ -37,7 +37,7 @@ uint8_t data::get_enumerated_label(){
         return enum_label;
 }
 
-std::vector<uint8_t> * data::get_featured_vector(){
+std::vector<uint8_t> * data::get_feature_vector(){
         return feature_vector;
 }
 
